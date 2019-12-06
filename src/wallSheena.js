3f1999710aac698eee486da6fa60e41136683f8b
TheAntiLuck <45467441+TheAntiLuck@users.noreply.github.com>
2019-12-06 08:31:31 -0800

    Add files via upload

---
+++ src/wallSheena.js
@@ -0,0 +1,41 @@
+function showAnswer1() {
+    var element = document.getElementById('Q1');
+    element.classList.remove("Q1");
+    element.classList.add("hide");
+};
+        
+function showAnswer2() {
+    var element = document.getElementById('Q2');
+    element.classList.remove("Q2");
+    element.classList.add("hide");
+};
+
+function showAnswer3() {
+    var element = document.getElementById('Q3');
+    element.classList.remove("Q3");
+    element.classList.add("hide");
+};
+
+function showAnswer4() {
+    var element = document.getElementById('Q4');
+    element.classList.remove("Q4");
+    element.classList.add("hide");
+};
+
+function showAnswer5() {
+    var element = document.getElementById('Q5');
+    element.classList.remove("Q5");
+    element.classList.add("hide");
+};
+
+function showAnswer6() {
+    var element = document.getElementById('Q6');
+    element.classList.remove("Q6");
+    element.classList.add("hide");
+};
+
+function showAnswer7() {
+    var element = document.getElementById('Q7');
+    element.classList.remove("Q7");
+    element.classList.add("hide");
+};
